f82c0451c19401511aa2c6df38f92c8849005c90
WillBoudreau <williamboudreau804@gmail.com>
2023-09-17 16:35:20 -0400

    Update Parkour1Behaviour.cs

--- Assets/Scripts/Parkour1Behaviour.cs
+++ Assets/Scripts/Parkour1Behaviour.cs
@@ -6,21 +6,27 @@ public class Parkour1Behaviour : MonoBehaviour
 {
     public GameObject button;
     public GameObject player;
-    public GameObject stop;
-    public GameObject parkour;
-    private float risespeed = 50f;
+    private Transform target;
+    private float speed = 15f;
+    // Code used is from Unity Documentation "Vector3.MoveTowards" 
     // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
-        
+        transform.position = new Vector3(5.0f, 4.0f, 95.0f);
+        target.transform.position = new Vector3(41.0f, 13.0f, 92.0f);
     }
 
     // Update is called once per frame
     void Update()
     {
+        var step = speed * Time.deltaTime;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, step);
         if (button.transform.position == player.transform.position)
         {
-            Transform.position.y = stop;
+            if (Vector3.Distance(transform.position, target.position) < 0.01f)
+            {
+                transform.position *= -1.0f;
+            }
         }
     }
 }
